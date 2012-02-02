286c7f2de9751b0acf56d09abfc26f0f54a0fcd6
Benjamin Rhodes <ben@pipelinedealsco.com>
2012-02-01 23:18:56 -0500

    just to make people hate things

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,20 @@
 class Search < ActiveRecord::Base
 
   def people
+    PersonSearch.new(self).people
+  end
+end
+
+class PersonSearch
+  def initialize(search)
+    @search = search
+  end
+
+  def q
+    @search.q
+  end
+
+  def people
     if q.present?
       Person.name_like(q)
     else
